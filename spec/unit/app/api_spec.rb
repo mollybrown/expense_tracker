bb2643152140f225d9d5102b884983863351ca81
Molly Brown <mollyrachelbrown@gmail.com>
2020-01-20 20:52:23 -0800

    ch05, specs complete for success

--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -1,11 +1,48 @@
 require_relative '../../../app/api'
+require 'rack/test'
 
 module ExpenseTracker
+  # Use value objects at layer boundaries (https://www.destroyallsoftware.com/talks/boundaries)
+  RecordResult = Struct.new(:success?, :expense_id, :error_message)
+
   RSpec.describe API do
+    include Rack::Test::Methods
+
+    def app
+      API.new(ledger: ledger)
+    end
+
+    # Create stand-in for instance of ExpenseTracker::Ledger class
+    let(:ledger) { instance_double('ExpenseTracker::API::Ledger') }
+
     describe 'POST /expenses' do
       context 'when the expense is sucessfully recorded' do
-        it 'returns the expense id'
-        it 'responds with a status code 200'
+        it 'returns the expense id' do
+          expense = { 'some' => 'data' }
+
+          # `allow` is a rspec-mock behavior that configures the behavior of the test double
+          # when the caller (API) invokes `record`, the double will return a new RecordResult instance indicating success
+          allow(ledger).to receive(:record).with(expense).and_return(RecordResult.new(true, 123, nil))
+
+          post '/expenses', JSON.generate(expense)
+
+          response = JSON.parse(last_response.body)
+          expect(response).to include('expense_id' => 123)
+        end
+        it 'responds with a status code 200' do
+          expense = { 'some' => 'data' }
+          allow(ledger).to receive(:record).with(expense).and_return(RecordResult.new(true, 123, nil))
+
+          post '/expenses', JSON.generate(expense)
+
+          expect(last_response.status).to eq(200)
+        end
+      end
+
+      context 'when the expense fails validation' do
+        it 'returns an error message'
+        it 'responds with a status code 422 (unprocessable entity)'
       end
     end
   end
+end
